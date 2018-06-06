a1c50b470a0a62340289259e5e981b92d17f5b5b
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-06 01:11:07 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: { message: "must not be blank" }
+  validates :title, presence: true
   validates :content, length: { minimum: 100, message: "too short" }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "must be Fiction or Non-Fiction" }
 end
