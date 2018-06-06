97b00f16f80e962c511e8774875631fe2f1399ca
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-06 01:14:18 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,10 +7,27 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
+  def edit
+    @author = Author.find(params[:id])
+  end
+
   def create
     @author = Author.create(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
 
-    redirect_to author_path(@author)
+  def update
+    @author = Author.find(params[:id])
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
