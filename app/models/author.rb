66b4af4df68325822ff86ec39330e9e4974968ea
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-06 00:39:46 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
-  validates :name, format: { without: /[0-9]/, message: "does not allow numbers" }
+  validates :name, presence: true, format: { without: /[0-9]/, message: "does not allow numbers" }
   validates :email, presence: true, uniqueness: true
   validates :phone_number, length: { is: 10 }
 end
