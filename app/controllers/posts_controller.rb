c9f25d7ae41bcb3162158fede8b5491cdd0cdecd
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-06 00:21:22 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
